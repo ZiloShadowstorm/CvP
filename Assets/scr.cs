ceaa6a91a81a081eac54de569a4c85eb432a24bf
Davy <ziloshadowstorm@gmail.com>
2017-04-25 18:32:57 +1200

    Basic Layout of the Game
    Window size set to 900 x 600.
    Camera not quite right yet. It's too wide and too much to the left. Will
    fix soon.

---
+++ Assets/scr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour {
+
+    public static bool visible = false;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().enabled = visible;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        visible = true;
+        GetComponent<Renderer>().enabled = visible;
+    }
+
+    private void OnMouseExit()
+    {
+        visible = false;
+        GetComponent<Renderer>().enabled = visible;
+    }
+}
